f0b0421a8a3dc545977a0d780ac6af79eccac209
dukizwe <mbagapro@gmail.com>
2022-06-20 10:29:10 +0200

    praparing for pulling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
           }
           const user = useSelector(userSelector)
           return (
-                    <SharedElement  id={"header"} style={{ height: 80 }}>
+                    <SharedElement  id={"header"} style={{ height: 10 }}>
                               <View style={styles.header}>
                                         <Image source={require('../../assets/wasili-icon.png')} style={{width: 100, height: 60, marginHorizontal: -20, marginVertical: 0, marginTop: 0, marginLeft: -30}} />
                                         <View style={styles.headerDesc}>
